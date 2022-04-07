02ec3e92663b146f7371672b71fd3ae3740dfe15
Leshnevskyi <leshnevskyi@gmail.com>
2022-04-07 13:59:54 +0300

    feat: a chess form and a game timer

--- index.js
+++ index.js
@@ -1,17 +1,97 @@
-import './index.css'
+import {Chart, registerables} from 'chart.js';
+import {titleCase} from 'title-case';
+
+import colors from 'tailwindcss/colors';
+
+import './index.css';
+
+Chart.register(...registerables);
+
+function getErrorMessage(element) {
+	const {
+  valueMissing: valueIsMissing,
+  patternMismatch: patternMismatches,
+	} = element.validity;
+	
+	switch (true) {
+  case valueIsMissing:
+  	return 'The field is required.';
+
+  case patternMismatches:
+  	return 'The provided value does not meet the field format.';
+
+  default:
+  	return '';
+	}
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  const {owlyForm} = document.forms
+  const {form} = document.forms;
+	const formElemenents = [...form.elements];
 
-  owlyForm.addEventListener('submit', event => {
-    event.preventDefault()
+	formElemenents.forEach(element => {
+  element.addEventListener('input', () => {
+  	element.classList.add('validated');
+  	element.setCustomValidity(getErrorMessage(element));
+  	element.reportValidity();
+  });
+	});
 
-    const entries = new FormData(owlyForm)
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
    
-    for (const [key, value] of entries) {
-      localStorage.setItem(key, value)
+    for (const [key, value] of formData) {
+      localStorage.setItem(key, value);
     }
 
-    window.alert('The form has been sent. Thank you!')
-  })
-})
+    form.remove();
+
+  const timerStopDate = Date.now() + Number(formData.get('gameTime')) * 60 * 1000;
+  const timerRoot = document.querySelector('.timerRoot');
+
+  const intervalId = setInterval(() => {
+  	const timeLeft = new Date(timerStopDate - Date.now());
+
+  	if (timeLeft < 0) {
+			clearInterval(intervalId);
+			timerRoot.textContent = `Time's up!`;
+
+			return;
+  	}
+
+  	timerRoot.textContent = timeLeft.toISOString().slice(14, 23);
+  }, 30);
+  });
+
+	const chessPieceKinds = ['pawn', 'rook', 'knight', 'bishop', 'queen', 'king'];
+
+	const chessPieces = chessPieceKinds.map((kind, index) => {
+		return {
+			name: titleCase(kind), 
+			count: document.querySelectorAll(`[data-chess-piece=${kind}]`).length,
+			color: colors.zinc[(index + 3) * 100],
+		};
+	});	
+
+	const context = document.getElementById('chart').getContext('2d');
+	new Chart(context, {
+  	type: 'bar',
+  	data: {
+    	labels: chessPieces.map(piece => piece.name),
+    	datasets: [{
+				label: 'Number of Pieces',
+      	data: chessPieces.map(piece => piece.count),
+      	backgroundColor: chessPieces.map(piece => piece.color),
+			}],
+  	},
+		options: {
+			scale: {
+				ticks: {
+					precision: 0
+				},
+			},
+		},
+	});
+});
